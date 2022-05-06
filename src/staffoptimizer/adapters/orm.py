a29bec795325cc8a0050dc1290720dc0456f4d57
Frederic Sautot <frederic.sautot@jellysmack.com>
2022-05-06 04:51:05 +0200

    Add mappers in orm

--- src/staffoptimizer/adapters/orm.py
+++ src/staffoptimizer/adapters/orm.py
@@ -13,21 +13,60 @@ https://docs.sqlalchemy.org/en/14/orm/mapping_api.html?#sqlalchemy.orm.mapper
 """
 
 
-from sqlalchemy import MetaData, Table, Column, Integer, String
-from sqlalchemy.orm import mapper
+from sqlalchemy import MetaData, Table, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import mapper, relationship
 
 from src.staffoptimizer.domain import model
 
 metadata = MetaData()
 
+staffoptimizers = Table(
+    "staffoptimizers",
+    metadata,
+    Column("run_id", String(255)),
+)
 tasks = Table(
     "tasks",
     metadata,
     Column("id", Integer, primary_key=True, autoincrement=True),
-    Column("title", String(255)),
+    Column("run_id", ForeignKey("staffoptimizer_runs.id")),
+    Column("reference", String(255)),
+    Column("status", String(255)),
+    Column("team", String(255)),
 )
-user = Table()
+users = Table(
+    "users",
+    metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("name", String(255)),
+    Column("editor_id", String(255)),
+    Column("csid", String(255)),
+    Column("esid", String(255)),
+)
+assignments = Table(
+    "assignments",
+    metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("task_id", ForeignKey("tasks.id")),
+    Column("user_id", ForeignKey("users.id")),
+)
 
 
 def start_mappers():
-    tasks_mapper = mapper(model.Task, tasks)
+    users_mapper = mapper(model.User, users)
+    tasks_mapper = mapper(
+        model.Task,
+        tasks,
+        properties={
+            "_assignments": relationship(
+                users_mapper,
+                secondary=assignments,
+                collection_class=set,
+            )
+        },
+    )
+    mapper(
+        model.StaffOptimizer,
+        staffoptimizers,
+        properties={"tasks": relationship(tasks_mapper)},
+    )
